31525356bd1641d928e9a19a8695a12799377f3c
harshpandloskar <harshpandloskar@gmail.com>
2018-12-10 22:39:54 +0530

    Added table creation

--- table.sql
+++ table.sql
@@ -1,6 +1,10 @@
 CREATE DATABASE IF NOT EXISTS `harshapp`;
 USE `harshapp`;
 
+-- SELECT * FROM `booking`;
+-- DROP TABLE `users`;
+-- DROP TABLE `booking`;
+
 CREATE TABLE `users` ( 
     UserNameID int(9) NOT NULL auto_increment,
     userName VARCHAR(40) NOT NULL,
@@ -10,22 +14,14 @@ CREATE TABLE `users` (
     PRIMARY KEY(UserNameID) 
 );
 
--- CREATE TABLE `users` (
---  `id` int(11) NOT NULL AUTO_INCREMENT,
---  `oauth_provider` enum('','facebook','google','twitter') COLLATE utf8_unicode_ci NOT NULL,
---  `oauth_uid` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
---  `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
---  `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
---  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
---  `gender` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
---  `locale` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
---  `cover` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
---  `picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
---  `link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
---  `created` datetime NOT NULL,
---  `modified` datetime NOT NULL,
---  PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-SELECT * FROM `users`;
+CREATE TABLE `booking` ( 
+    UserNameID int(9) NOT NULL auto_increment,
+    userName VARCHAR(40) NOT NULL,
+    userEmail VARCHAR(40) NOT NULL,
+    bookingID VARCHAR(100) NOT NULL,
+    seatNum VARCHAR(100) NOT NULL,
+    eventName VARCHAR(100) NOT NULL,
+    eventDay VARCHAR(40) NOT NULL,
+    eventTime VARCHAR(40) NOT NULL,
+    PRIMARY KEY(UserNameID) 
+);
